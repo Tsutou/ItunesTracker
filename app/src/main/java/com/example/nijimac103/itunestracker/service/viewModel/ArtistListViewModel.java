0591f5f81a65c4fc34bf521c4820309eeb443fdf
Tsutou <tsutoutakehara@gmail.com>
2018-05-31 14:46:27 +0900

    Fix

--- app/src/main/java/com/example/nijimac103/itunestracker/service/viewModel/ArtistListViewModel.java
+++ app/src/main/java/com/example/nijimac103/itunestracker/service/viewModel/ArtistListViewModel.java
@@ -1,4 +1,26 @@
 package com.example.nijimac103.itunestracker.service.viewModel;
 
-public class ArtistListViewModel {
+import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
+import android.arch.lifecycle.LiveData;
+
+import com.example.nijimac103.itunestracker.service.model.Artist;
+import com.example.nijimac103.itunestracker.service.repository.ArtistRepository;
+
+import java.util.List;
+
+public class ArtistListViewModel extends AndroidViewModel {
+
+    private final LiveData<List<Artist>> artistListObservable;
+    private int LIMIT = 30;
+
+    public ArtistListViewModel(Application application) {
+        super(application);
+
+        artistListObservable = ArtistRepository.getInstance().getArtistList("AliciaKeys", "MusicVideo", LIMIT);
+    }
+
+    public LiveData<List<Artist>> getArtistListObservable(){
+        return artistListObservable;
+    }
 }
