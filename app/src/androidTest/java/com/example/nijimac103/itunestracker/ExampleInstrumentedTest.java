d2d366a748125201ed05ad9ea26a98db6ab5e6b3
Tsutou <tsutoutakehara@gmail.com>
2019-12-08 03:38:06 +0900

    refactor

--- app/src/androidTest/java/com/example/nijimac103/itunestracker/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/nijimac103/itunestracker/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("jp.co.geisha.itunestracker", appContext.getPackageName());
+        assertEquals("jp.co.geisha.diggin", appContext.getPackageName());
     }
 }
