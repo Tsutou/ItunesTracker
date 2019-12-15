2c5437fa09a337935baa027031464ee0baf150f3
Tsutou <tsutoutakehara@gmail.com>
2019-12-15 21:48:37 +0900

    add callback

--- app/src/main/java/jp/co/geisha/diggin/callback/MusicVideoClickCallback.kt
+++ app/src/main/java/jp/co/geisha/diggin/callback/MusicVideoClickCallback.kt
@@ -1,7 +1,9 @@
 package jp.co.geisha.diggin.callback
 
 import jp.co.geisha.diggin.api.entity.ItunesData
+import jp.co.geisha.diggin.api.entity.YouTubeResponse
 
 interface MusicVideoClickCallback {
     fun onClick(itunesData: ItunesData)
+    fun onClick(youtubeData: YouTubeResponse.Data)
 }
