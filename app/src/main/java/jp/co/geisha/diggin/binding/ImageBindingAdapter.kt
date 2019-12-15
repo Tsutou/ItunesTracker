297cb3fd96200649a0d8c7d0e56cccb86fd49ea4
Tsutou <tsutoutakehara@gmail.com>
2019-12-15 21:48:12 +0900

    fix imageview binding

--- app/src/main/java/jp/co/geisha/diggin/binding/ImageBindingAdapter.kt
+++ app/src/main/java/jp/co/geisha/diggin/binding/ImageBindingAdapter.kt
@@ -7,8 +7,14 @@ import jp.co.geisha.diggin.util.PicassoUtil
 object ImageBindingAdapter {
 
     @JvmStatic
-    @BindingAdapter("circleImageUrl")
+    @BindingAdapter("imageUrl")
     fun loadImage(view: ImageView, imageUrl: String) {
+        PicassoUtil.loadPicasso(imageUrl, view)
+    }
+
+    @JvmStatic
+    @BindingAdapter("circleImageUrl")
+    fun loadImageWithCircleTransform(view: ImageView, imageUrl: String) {
         PicassoUtil.loadPicassoWithCropCircleTransformation(imageUrl, view)
     }
 }
