4da62faf7a5311fc272c458b31d5ed13092b2394
Tsutou <tsutoutakehara@gmail.com>
2019-12-20 02:38:11 +0900

    refacor

--- app/src/main/java/jp/co/geisha/diggin/util/PicassoUtil.kt
+++ app/src/main/java/jp/co/geisha/diggin/util/PicassoUtil.kt
@@ -3,13 +3,14 @@ package jp.co.geisha.diggin.util
 import android.graphics.drawable.Drawable
 import android.widget.ImageView
 import com.squareup.picasso.Picasso
+import jp.co.geisha.diggin.R
 import jp.wasabeef.picasso.transformations.CropCircleTransformation
 import jp.wasabeef.picasso.transformations.CropSquareTransformation
 
 object PicassoUtil {
 
     fun loadPicasso(url: String, view: ImageView)
-            = Picasso.get().load(url).into(view)
+            = Picasso.get().load(url).placeholder(R.drawable.main_theme).into(view)
 
     fun loadPicasso(url: String, error: Drawable, view: ImageView)
             = Picasso.get().load(url).placeholder(error).error(error).into(view)
--- app/src/main/java/jp/co/geisha/diggin/view/activity/MainActivity.kt
+++ app/src/main/java/jp/co/geisha/diggin/view/activity/MainActivity.kt
@@ -3,7 +3,7 @@ package jp.co.geisha.diggin.view.activity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.Toast
-import com.google.android.youtube.player.YouTubeStandalonePlayer
+import com.google.android.youtube.player.*
 
 import jp.co.geisha.diggin.R
 import jp.co.geisha.diggin.api.entity.ItunesData
@@ -13,6 +13,7 @@ import jp.co.geisha.diggin.ARTIST_TABS_FRAGMENT_TAG
 import jp.co.geisha.diggin.BuildConfig
 import jp.co.geisha.diggin.api.entity.YouTubeResponse
 import jp.co.geisha.diggin.view.fragment.MusicVideoListFragment
+import java.lang.IllegalStateException
 
 class MainActivity : AppCompatActivity() {
 
@@ -33,16 +34,15 @@ class MainActivity : AppCompatActivity() {
 
     fun show(itunesData: ItunesData) {
         if (itunesData.previewUrl.isNotEmpty()) {
-            val intent = ItunesVideoViewActivity.createIntent(this,itunesData.previewUrl)
+            val intent = ItunesVideoViewActivity.createIntent(this, itunesData.previewUrl)
             startActivity(intent)
         } else {
             Toast.makeText(applicationContext, "This Video has not Preview", Toast.LENGTH_SHORT).show()
         }
     }
 
-    fun show(youtubeData: YouTubeResponse.Data){
+    fun show(youtubeData: YouTubeResponse.Data) {
         if (youtubeData.id.videoId.isNotEmpty()) {
-
             val intent = YouTubeStandalonePlayer.createVideoIntent(
                     this,
                     BuildConfig.YOUTUBE_API_KEY,
@@ -53,7 +53,7 @@ class MainActivity : AppCompatActivity() {
             )
             startActivity(intent)
         } else {
-            Toast.makeText(applicationContext, "This Video has not Preview", Toast.LENGTH_SHORT).show()
+            Toast.makeText(applicationContext, "This Video has not id", Toast.LENGTH_SHORT).show()
         }
     }
 
